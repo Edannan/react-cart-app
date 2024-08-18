681f37f83c63fa05fb1444ac33247dd2b5c0da38
Edannan <emmanueldansoannan@gmail.com>
2024-08-18 18:42:53 +0000

    images and data added

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const meals = [
+  {
+    id: "m1",
+    price: 16,
+    title: "Burger",
+    description: "Sink your teeth into our juicy, flame-grilled burger, crafted from premium, hand-pressed beef. Nestled in a soft, toasted bun, this burger is topped with fresh lettuce, ripe tomato, crisp pickles, and melted cheese. Add our signature sauce for an extra burst of flavor. Perfectly cooked to order!",
+    image: "/images/burger.jpg"
+  },
+  {
+    id: "m2",
+    price: 21,
+    title: "Fried Chicken",
+    description: "Golden-brown, perfectly seasoned, and fried to crispy perfection, our fried chicken is juicy on the inside and irresistibly crunchy on the outside. Each piece is marinated in our secret blend of spices, delivering a mouth-watering flavor that will have you coming back for more. Served hot and fresh!",
+    image: "/images/friedchicken.jpg"
+  },
+  {
+    id: "m3",
+    price: 20,
+    title: "Fries and Dipping Sauce",
+    description: "Golden, crispy fries, perfectly seasoned and cooked to a satisfying crunch. Paired with your choice of dipping sauce, from tangy ketchup and creamy mayo to zesty garlic aioli and smoky barbecue. Whether you're enjoying them as a side or a snack, these fries are the perfect companion to any meal.",
+    image: "/images/friesanddippingsauce.jpg"
+  },
+  {
+    id: "m4",
+    price: 23,
+    title: "Pizza",
+    description: "Indulge in our artisanal pizza, made with hand-stretched dough, fresh ingredients, and baked to perfection in a traditional wood-fired oven. Topped with tangy tomato sauce, melted mozzarella, and your choice of premium toppings, each slice offers a perfect balance of crispy crust, gooey cheese, and rich flavors.",
+    image: "/images/pizza.jpg"
+  }
+];
+
+export default meals;
