a164a29d97a01189d1e106f119dbbfe065a62261
Edannan <emmanueldansoannan@gmail.com>
2024-08-22 07:33:21 +0000

    image data fixed

--- src/data.js
+++ src/data.js
@@ -1,3 +1,15 @@
+import burgerImage from './images/burger.jpg';
+import friedChickenImage from './images/friedchicken.jpg';
+import friesImage from './images/friesanddippingsauce.jpg';
+import pizzaImage from './images/pizza.jpg';
+import pastaAlfredoImage from './images/pastaalfredo.jpg';
+import caesarSaladImage from './images/caesarsalad.jpg';
+import bbqRibsImage from './images/bbqribs.jpg';
+import margheritaPizzaImage from './images/magheritapizza.jpg';
+
+
+
+
 const meals = [
   {
     id: "m1",
@@ -5,7 +17,7 @@ const meals = [
     title: "Burger",
     description:
       "Sink your teeth into our juicy, flame-grilled burger, crafted from premium, hand-pressed beef. Nestled in a soft, toasted bun, this burger is topped with fresh lettuce, ripe tomato, crisp pickles, and melted cheese. Add our signature sauce for an extra burst of flavor. Perfectly cooked to order!",
-    image: "/images/burger.jpg",
+    image: burgerImage,
   },
   {
     id: "m2",
@@ -13,7 +25,7 @@ const meals = [
     title: "Fried Chicken",
     description:
       "Golden-brown, perfectly seasoned, and fried to crispy perfection, our fried chicken is juicy on the inside and irresistibly crunchy on the outside. Each piece is marinated in our secret blend of spices, delivering a mouth-watering flavor that will have you coming back for more. Served hot and fresh!",
-    image: "/images/friedchicken.jpg",
+    image: friedChickenImage,
   },
   {
     id: "m3",
@@ -21,7 +33,7 @@ const meals = [
     title: "Fries and Dipping Sauce",
     description:
       "Golden, crispy fries, perfectly seasoned and cooked to a satisfying crunch. Paired with your choice of dipping sauce, from tangy ketchup and creamy mayo to zesty garlic aioli and smoky barbecue. Whether you're enjoying them as a side or a snack, these fries are the perfect companion to any meal.",
-    image: "/images/friesanddippingsauce.jpg",
+    image: friesImage,
   },
   {
     id: "m4",
@@ -29,7 +41,7 @@ const meals = [
     title: "Pizza",
     description:
       "Indulge in our artisanal pizza, made with hand-stretched dough, fresh ingredients, and baked to perfection in a traditional wood-fired oven. Topped with tangy tomato sauce, melted mozzarella, and your choice of premium toppings, each slice offers a perfect balance of crispy crust, gooey cheese, and rich flavors.",
-    image: "/images/pizza.jpg",
+    image: pizzaImage,
   },
   {
     id: "m5",
@@ -37,7 +49,7 @@ const meals = [
     title: "Pasta Alfredo",
     description:
       "Creamy, rich, and absolutely delicious, our Pasta Alfredo is made with tender fettuccine noodles tossed in a velvety Alfredo sauce. Finished with freshly grated Parmesan cheese and a sprinkle of parsley, this dish is comfort food at its finest.",
-    image: "/images/pastaalfredo.jpg",
+    image: pastaAlfredoImage,
   },
   {
     id: "m6",
@@ -45,7 +57,7 @@ const meals = [
     title: "Caesar Salad",
     description:
       "Crisp romaine lettuce, crunchy croutons, and freshly grated Parmesan cheese tossed in a creamy Caesar dressing. This classic salad is topped with tender grilled chicken breast for a light yet satisfying meal.",
-    image: "/images/caesarsalad.jpg",
+    image: caesarSaladImage,
   },
   {
     id: "m7",
@@ -53,7 +65,7 @@ const meals = [
     title: "BBQ Ribs",
     description:
       "Slow-cooked and smothered in our house-made barbecue sauce, these tender ribs are fall-off-the-bone delicious. Served with a side of coleslaw and buttery cornbread, they're a true taste of comfort and tradition.",
-    image: "/images/bbqribs.jpg",
+    image: bbqRibsImage,
   },
   {
     id: "m8",
@@ -61,7 +73,7 @@ const meals = [
     title: "Margherita Pizza",
     description:
       "A classic Margherita pizza made with a crispy, thin crust, topped with a simple yet flavorful combination of fresh tomatoes, mozzarella cheese, and basil leaves. Drizzled with extra virgin olive oil and baked to perfection.",
-    image: "/images/magheritapizza.jpg",
+    image: margheritaPizzaImage,
   },
 ];
 
